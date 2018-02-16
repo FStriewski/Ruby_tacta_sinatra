6114c0ea54733fc6fdb67836a47094437c4ea8ee
FStriewski <fstriewski@outlook.com>
2018-02-16 17:21:31 +0100

    Add index.erb, add show.erb, add context/:index, add context/:show

--- tacta_sn.rb
+++ tacta_sn.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
+require './contacts_file'
 
 get '/' do
   "<h1>Tacta Contact Manager</h1>"
+end
+
+# GET /contacts
+get '/contacts' do
+   @contacts = read_contacts
+   erb :'contacts/index'
+end
+
+
+# GET /contacts/:index
+get '/contacts/:index' do
+  @index = params[:index].to_i
+  contacts = read_contacts
+  @contact = contacts[@index]
+  erb :'contacts/show'
 end
